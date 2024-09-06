a80a2eeabea30a4f927c4363c79618a074768dee
Cain <134607924+CainC0de@users.noreply.github.com>
2024-09-06 12:56:03 -0300

    Update CameraWebServer.ino

--- CameraWebServer.ino
+++ CameraWebServer.ino
@@ -1,14 +1,6 @@
 #include "esp_camera.h"
 #include <WiFi.h>
 
-//
-// WARNING!!! Make sure that you have either selected ESP32 Wrover Module,
-//            or another board which has PSRAM enabled
-//
-
-// Select camera model
-//#define CAMERA_MODEL_WROVER_KIT
-//#define CAMERA_MODEL_M5STACK_PSRAM
 #define CAMERA_MODEL_AI_THINKER
 
 const char* ssid = "REPLACE_WITH_YOUR_SSID";
